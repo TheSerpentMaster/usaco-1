05e55ccab94110bdf039bfb0e81c9e06e51ef386
CyberShark <dragonboyiscool029@gmail.com>
2025-07-02 19:44:00 -0400

    yayy another one bites the dust

--- 2025/hw_061125.py
+++ 2025/hw_061125.py
@@ -4,10 +4,69 @@ N = int(input())
 metals = input().split()
 K = int(input())
 
-recipes = {}
+total_n_metals = 0
 
-for metal in range(len(metals)):
-    metals[metal] = int(metals[metal])
+recipes = {}
+inventory = {}
+need = {N: 1}
+
+def missing(need: dict, inventory: dict):
+    missing = {}
+
+    for metal in need.keys():
+        if metal not in inventory.keys():
+            missing[metal] = need[metal]
+        
+        elif inventory[metal] < need[metal]:
+            missing[metal] = need[metal] - inventory[metal]
+        
+    return missing
+
+def update_need(need: dict, recipes: dict, missing):
+    if missing != {}:
+        for metal in list(missing.keys()):
+            if metal in recipes.keys():
+                for element in recipes[metal]:
+                    if element in need.keys():
+                        need[element] += missing[metal]
+                    
+                    else:
+                        need[element] = missing[metal]
+
+            else:
+                return False
+
+            need[metal] -= missing[metal]
+
+            if need[metal] == 0:
+                del need[metal]
+
+        return need
+    
+    return need
+
+def inventory_update(current_need: dict, inventory: dict):
+    for metal in current_need.keys():
+        if metal in inventory.keys():
+            if inventory[metal] < current_need[metal]:
+                del inventory[metal]
+                
+            else:
+                inventory[metal] -= current_need[metal]
+    
+    return inventory
+
+for element in range(0, len(metals) + 1):
+    inventory[element] = int(metals[element - 1])
+
+for i in range(N):
+    for element in range(len(metals)):
+        if metals[element] == i:
+            if i not in inventory.keys():
+                inventory[i] = 1
+            
+            else:
+                inventory[i] += 1
 
 for _ in range(K):
     recipe = input().split()
@@ -17,3 +76,19 @@ for _ in range(K):
     
     recipes[recipe[0]] = recipe[2:]
 
+current_need = need.copy()
+
+while current_need:
+    inventory_copy = inventory.copy()
+    missing_metals = missing(current_need, inventory_copy)
+    #print(missing_metals)
+
+    if missing_metals != {}:
+        current_need = update_need(current_need, recipes, missing_metals)
+        #print(current_need)
+
+    else:
+        inventory = inventory_update(current_need, inventory_copy)
+        total_n_metals += 1
+
+print(total_n_metals)
--- test.py
+++ test.py
@@ -2,17 +2,28 @@
 
 import math
 
-def factorize(num: int):
-    i = 1
-    number = num
-    factors = []
+def update_need(need: dict, recipes: dict, missing):
+    if missing != {}:
+        for metal in list(missing.keys()):
+            print(recipes)
+            if metal in recipes.keys():
+                for element in recipes[metal]:
+                    if element in need.keys():
+                        need[element] += missing[metal]
+                    
+                    else:
+                        need[element] = missing[metal]
 
-    while i <= math.sqrt(num):
-        if number % i == 0:
-            factors.append(i)
-            factors.append(int(num / i))
-        i += 1
+            else:
+                return False
+
+            need[metal] -= missing[metal]
+
+            if need[metal] == 0:
+                del need[metal]
+
+        return need
     
-    return factors
+    return need
 
-print(factorize(91))
+print(update_need({3: 1, 4: 1}, {2: [1], 3: [2], 4: [3]}, {3: 1, 4: 1}))  # prints {1: 1, 2: 2}
