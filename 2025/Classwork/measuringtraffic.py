0a57874bbdc9626dfd1622797f6127efb94b78c8
CyberShark <dragonboyiscool029@gmail.com>
2025-07-16 21:29:42 -0400

    more again *guh)

---
+++ 2025/Classwork/measuringtraffic.py
@@ -0,0 +1,6 @@
+# USACO February US Open Contest Bronze #3: Measuring Traffic
+
+trafficinput = ''
+
+with open("traffic.in", 'r+') as input_:
+    trafficinput = input_.readlines().strip().split()
--- 2025/hw_072325.py
+++
@@ -1,3 +0,0 @@
-with open("traffic.in", 'r+') as input_:
-    input_.readlines().strip().split()
-
